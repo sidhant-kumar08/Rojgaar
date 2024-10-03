38a034a7446921fd788a019fa3b28571c730e648
Sidhant Kumar <162786760+sidhant-kumar08@users.noreply.github.com>
2024-10-03 16:13:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(urlencoded({extended: true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://rojgaar.vercel.app/',
     credentials: true,
 }))
 
